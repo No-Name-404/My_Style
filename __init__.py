f2ddc95b75dfcac487e47dfe2e137e27b552a46b
Mohamed - kainai <56244233+No-Name-404@users.noreply.github.com>
2020-01-07 22:50:23 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 from V7xStyle.graphics import Animation,Style,Text
-from V7xStyle.graphics import W,P,Y,C,Bl,G,B,R
+from V7xStyle.graphics import BL,W,P,Y,C,Bl,G,B,R
+from V7xStyle.graphics import Color_C
